4c4dd59ac4d7787cf4ff98d49851753d4dedf9c1
hooni <jhlee909090@gmail.com>
2025-07-09 14:23:19 +0900

    feat/#62 일정삭제 레포지토리 구현

--- Projects/Calendar/CalendarDomain/Sources/Repositories/CalendarRepositoryProtocol.swift
+++ Projects/Calendar/CalendarDomain/Sources/Repositories/CalendarRepositoryProtocol.swift
@@ -10,7 +10,11 @@ import Foundation
 
 public protocol CalendarRepositoryProtocol {
     func getSchedules(startDate: String, endDate: String) async throws -> [DailySchedule]
+<<<<<<< HEAD
     func createSchedule(request: CreateScheduleRequest) async throws -> Schedule
     func updateSchedule(id: String, request: UpdateScheduleRequest) async throws -> Schedule  
     func deleteSchedule(id: String) async throws
+=======
+    func deleteSchedule(id: String) async throws -> String
+>>>>>>> 7a5f5bf (feat/#62 일정삭제 레포지토리 구현)
 }
