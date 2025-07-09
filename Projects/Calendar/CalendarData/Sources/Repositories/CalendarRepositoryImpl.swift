8a5e546fe8d43874a7a727feeeadd62087b87ca5
hooni <jhlee909090@gmail.com>
2025-07-09 21:04:58 +0900

    fix/#62 린트오류수정

--- Projects/Calendar/CalendarData/Sources/Repositories/CalendarRepositoryImpl.swift
+++ Projects/Calendar/CalendarData/Sources/Repositories/CalendarRepositoryImpl.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2025 com.noweekend. All rights reserved.
 //
 
-import Foundation
 import CalendarDomain
+import Foundation
 import NWNetwork
 import Utils
 
@@ -62,7 +62,9 @@ public final class CalendarRepositoryImpl: CalendarRepositoryProtocol {
         
         let encoder = JSONEncoder()
         let data = try encoder.encode(requestDTO)
-        let parameters = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        guard let parameters = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            throw NetworkError.networkError("Failed to serialize parameters")
+        }
         
         let response: CreateScheduleResponseDTO = try await networkService.post(
             endpoint: "/schedule",
@@ -87,7 +89,9 @@ public final class CalendarRepositoryImpl: CalendarRepositoryProtocol {
         
         let encoder = JSONEncoder()
         let data = try encoder.encode(requestDTO)
-        let parameters = try JSONSerialization.jsonObject(with: data) as! [String: Any]
+        guard let parameters = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+            throw NetworkError.networkError("Failed to serialize parameters")
+        }
         
         let response: UpdateScheduleAPIResponseDTO = try await networkService.put(
             endpoint: "/schedule/\(id)",
