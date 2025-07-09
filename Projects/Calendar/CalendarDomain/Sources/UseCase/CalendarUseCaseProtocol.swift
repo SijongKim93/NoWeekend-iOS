e4bc589f3faec130ff14a1ec3e0b28fbefe8da1f
hooni <jhlee909090@gmail.com>
2025-07-09 20:08:42 +0900

    feat/#62 - 수정삭제 Usecase 구현

--- Projects/Calendar/CalendarDomain/Sources/UseCase/CalendarUseCaseProtocol.swift
+++ Projects/Calendar/CalendarDomain/Sources/UseCase/CalendarUseCaseProtocol.swift
@@ -12,5 +12,25 @@ public protocol CalendarUseCaseProtocol {
     func getWeeklySchedules(for date: Date) async throws -> [DailySchedule]
     func getMonthlySchedules(for date: Date) async throws -> [DailySchedule]
     func getSchedulesForDateRange(startDate: Date, endDate: Date) async throws -> [DailySchedule]
-    func deleteSchedule(id: String) async throws -> String
+    func createSchedule(
+        title: String,
+        date: Date,
+        startTime: Date,
+        endTime: Date,
+        category: ScheduleCategory,
+        temperature: Int,
+        allDay: Bool,
+        alarmOption: AlarmOption
+    ) async throws -> Schedule
+    func updateSchedule(
+        id: String,
+        title: String,
+        startTime: Date,
+        endTime: Date,
+        category: ScheduleCategory,
+        temperature: Int,
+        allDay: Bool,
+        alarmOption: AlarmOption
+    ) async throws -> Schedule
+    func deleteSchedule(id: String) async throws
 }
