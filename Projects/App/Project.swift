8433bc62340db7ef2bd537025faf5f7cd7b1f8f9
hooni <jhlee909090@gmail.com>
2025-07-10 21:22:11 +0900

    fix/#64 정상화

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -1,45 +1,3 @@
-//import ProjectDescription
-//import ProjectDescriptionHelpers
-//
-//let project = Project.make(
-//    name: "App",
-//    targets: [
-//        .app(
-//            name: "App",
-//            bundleId: BundleID.app,
-//            infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
-//            sources: ["Sources/**"],
-//            resources: ["Resources/**"],
-//            dependencies: [
-//                .Home.feature,
-//                .Profile.feature,
-//                .Calendar.feature,
-//                .Onboarding.feature,
-//                .Login.feature,
-//                .Bridge.dataBridge,
-//                .Shared.designSystem,
-                .Shared.utils
-            ],
-            settings: .settings(
-                base: [
-                    "OTHER_LDFLAGS": "$(inherited) -ObjC"
-                ]
-            )
-        )
-    ],
-//    resourceSynthesizers: [
-//        .custom(
-//            name: "Fonts",
-//            parser: .fonts,
-//            extensions: ["ttf", "otf"]
-//        ),
-//        .custom(
-//            name: "Strings",
-//            parser: .strings,
-//            extensions: ["strings"]
-//        )
-//    ]
-//)
 import ProjectDescription
 import ProjectDescriptionHelpers
 
@@ -78,4 +36,3 @@ let project = Project.make(
         )
     ]
 )
-
