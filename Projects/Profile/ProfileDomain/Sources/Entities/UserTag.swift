ff8e5e0378626ed9f1c7a922aabab22037e9faaa
SijongKim93 <rlatlwhd456@gmail.com>
2025-07-11 15:02:54 +0900

    feat/#71-Profile Model, DTO, Interface 구현

---
+++ Projects/Profile/ProfileDomain/Sources/Entities/UserTag.swift
@@ -0,0 +1,53 @@
+//
+//  UserTag.swift
+//  ProfileDomain
+//
+//  Created by SiJongKim on 7/11/25.
+//  Copyright © 2025 com.noweekend. All rights reserved.
+//
+
+import Foundation
+
+
+public struct UserTag: Equatable {
+    public let id: String
+    public let content: String
+    public let userId: String
+    public let selected: Bool
+    
+    public init(id: String, content: String, userId: String, selected: Bool) {
+        self.id = id
+        self.content = content
+        self.userId = userId
+        self.selected = selected
+    }
+}
+
+public struct UserTagsResponse: Equatable {
+    public let selectedBasicTags: [UserTag]
+    public let unselectedBasicTags: [UserTag]
+    public let selectedCustomTags: [UserTag]
+    public let unselectedCustomTags: [UserTag]
+    
+    public init(
+        selectedBasicTags: [UserTag],
+        unselectedBasicTags: [UserTag],
+        selectedCustomTags: [UserTag],
+        unselectedCustomTags: [UserTag]
+    ) {
+        self.selectedBasicTags = selectedBasicTags
+        self.unselectedBasicTags = unselectedBasicTags
+        self.selectedCustomTags = selectedCustomTags
+        self.unselectedCustomTags = unselectedCustomTags
+    }
+}
+
+public struct UserTagsUpdateRequest: Equatable {
+    public let addScheduleTags: [String]
+    public let deleteScheduleTags: [String]
+    
+    public init(addScheduleTags: [String], deleteScheduleTags: [String]) {
+        self.addScheduleTags = addScheduleTags
+        self.deleteScheduleTags = deleteScheduleTags
+    }
+}
