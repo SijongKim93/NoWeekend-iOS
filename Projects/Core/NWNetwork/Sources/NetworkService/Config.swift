ed6d28f98b5345c163eca284dbdf4ddfaa063142
SijongKim93 <rlatlwhd456@gmail.com>
2025-07-12 15:17:38 +0900

    fix/#71-VacationView 수정

--- Projects/Core/NWNetwork/Sources/NetworkService/Config.swift
+++ Projects/Core/NWNetwork/Sources/NetworkService/Config.swift
@@ -1,8 +1,8 @@
 //
 //  Config.swift
-//  Coordinator
+//  NWNetwork
 //
-//  Created by 김나희 on 7/6/25.
+//  Created by SiJongKim on 7/9/25.
 //  Copyright © 2025 com.noweekend. All rights reserved.
 //
 
@@ -11,6 +11,8 @@ import Foundation
 public enum Config {
     public static let baseURL = "https://noweekend.store/api/v1"
     
-    public static let googleClientID = "753472005020-3ferbo60566boe71vebq2vd2g4gc0g9h.apps.googleusercontent.com"
-    public static let googleServerClientID = "753472005020-94meo7qor2952p66a95v78bdhddcs5v0.apps.googleusercontent.com"
+    public static let googleClientID = "178939662981-k1frtfi591an22rs8fubv33i64b19uik.apps.googleusercontent.com"
+    public static let googleServerClientID = "178939662981-irg5o685k19p6evbr1pnfnok5scdaja8.apps.googleusercontent.com"
+    
+    public static let tempAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ2MS0yMDI1MDcxMjA5MzU0NS00Yzg4Nzc3MDhhYjM0MmMzYWE1YzFkMmNiOWZkZDg0NCIsImlzcyI6Imh0dHBzOi8vd3d3Lm5vd2Vla2VuZC5jb20iLCJpYXQiOjE3NTIyODA1NDUsImV4cCI6MTc1MjM2Njk0NX0.nFarhRQIJRU9pDU5i0esr49zcdSB1j7rHxN4PLTHINQ"
 }
